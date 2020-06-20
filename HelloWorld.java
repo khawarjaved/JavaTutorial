67cf10f095a497160f873e666b93320b956e0874
khawa <khavarjk@gmail.com>
2020-06-21 00:48:40 +0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello World...jk");
+        System.out.print("Hello World...jk khawar");
     }
 }
