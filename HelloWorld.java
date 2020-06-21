79d27d72762a5d9cb365a800a82a4177da583dcb
khawa <khavarjk@gmail.com>
2020-06-21 15:49:29 +0500

    int and float variable declear

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World...jk khawar");
-//using string 
+//use variable string 
 String name ="Khawar Javed";
 System.out.println(name);
+//use variable int and float number
+int a = 50;
+float b = 47.99f;
+System.out.println(b);
+
     }
 }
