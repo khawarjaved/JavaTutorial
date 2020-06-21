30b677f07585e1851eb131e0276308aa211bacdc
khawa <khavarjk@gmail.com>
2020-06-21 16:30:28 +0500

    Primitive type

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,13 +4,28 @@ public class HelloWorld {
 //use variable string 
 String name ="Khawar Javed";
 System.out.println(name);
-//use variable int and float number
+//use variable int , float and blooean type declared 
 int a = 50;
 float b = 47.99f;
 boolean isAdult = false;
 System.out.println(a);
 System.out.println(b);
 System.out.println(isAdult);
-
+/*
+Rules of constracting name of variables in java.
+1.Can contain digits,underscore,doller sign,letters.
+2.Should begin with a letter,$ or _
+3.Java is case sensitive language which means that khawar and Khawar 
+are two diffirent variables altogether.
+4.Should not contain whitespaces.
+5.You cannot use resevred keywords from java.
+*/
+/*Two types of Data Types.
+1.Primitive byte(1 byte)short(2 bytes)int(4 bytes)long (8bytes)float(4bytes)
+double(8bytes)boolean(1 bit)char (2 bytes)
+2.Non Primitive or Reference Data type.
+*/
+byte u = 65;
+System.out.println(u);
     }
 }
