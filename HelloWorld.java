7188ead5a61c1400525549cf185b227625355b36
khawa <khavarjk@gmail.com>
2020-06-21 15:57:09 +0500

    boolean variable declared

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,7 +7,10 @@ System.out.println(name);
 //use variable int and float number
 int a = 50;
 float b = 47.99f;
+boolean isAdult = false;
+System.out.println(a);
 System.out.println(b);
+System.out.println(isAdult);
 
     }
 }
