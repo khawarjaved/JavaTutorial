50b191ed4fd8ddf93612127c3ea780cd12e5d4fd
khawa <khavarjk@gmail.com>
2020-07-01 16:57:07 +0500

    calculator

--- HelloWorld.java
+++ HelloWorld.java
@@ -149,7 +149,7 @@ try{
 }
 System.out.println("masoom");*/
 //System.out.println(sum( 6,8));
-float number_1, number_2;
+/*float number_1, number_2;
 System.out.println("Enter first number");
 Scanner scan = new Scanner(System.in);
 number_1 = scan.nextFloat();
@@ -185,7 +185,50 @@ switch(input){
     System.out.println(number_1 / number_2);
     break;
     default:
-    System.out.println("invalid input");
+    System.out.println("invalid input");*/
+    
+
+ {
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+
+        // nextDouble() reads the next double from the keyboard
+        double first = reader.nextDouble();
+        double second = reader.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = reader.next().charAt(0);
+
+        double result;
+
+        switch(operator)
+        {
+            case '+':
+                result = first + second;
+                break;
+
+            case '-':
+                result = first - second;
+                break;
+
+            case '*':
+                result = first * second;
+                break;
+
+            case '/':
+                result = first / second;
+                break;
+
+            // operator doesn't match any case constant (+, -, *, /)
+            default:
+                System.out.printf("Error! operator is not correct");
+                return;
+        }
+
+        System.out.printf("%.1f %c %.1f = %.1f", first, operator, second, result);
+    }
 }
 }
-}
+
+//}
