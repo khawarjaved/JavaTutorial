231cbbfda99b510e5657c62ec0739cfdb85d1c0c
khawa <khavarjk@gmail.com>
2020-07-02 16:43:20 +0500

    Variable Tyep string int boolean and float

---
+++ Condition.java
@@ -0,0 +1,16 @@
+
+public class Condition {
+
+    public static void main(String[] args) {
+
+        String name = "Khawa r Javed";
+        System.out.println(name);
+        int a = 50;
+        float b = 47.99f;
+        boolean isAdult = false;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(isAdult);
+
+    }
+}
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,7 @@
 
 public class HelloWorld {
-    
+
     public static void main(String[] args) {
         System.out.println("Hello World...jk khawar");
-
-}
+    }
 }
