fa8559b4ae89a137b82a952a2e9460d4aef35615
khawa <khavarjk@gmail.com>
2020-07-02 17:05:58 +0500

    primitive and non primitive data types.

--- Condition.java
+++ Condition.java
@@ -3,7 +3,8 @@ public class Condition {
 
     public static void main(String[] args) {
 
-        String name = "Khawa r Javed";
+//use variable int , float and blooean type declared 
+        String name = "Khawar Javed";
         System.out.println(name);
         int a = 50;
         float b = 47.99f;
@@ -11,6 +12,15 @@ public class Condition {
         System.out.println(a);
         System.out.println(b);
         System.out.println(isAdult);
+        /*
+Rules of constracting name of variables in java.
+1.Can contain digits,underscore,doller sign,letters.
+2.Should begin with a letter,$ or _
+3.Java is case sensitive language which means that khawar and Khawar 
+are two diffirent variables altogether.
+4.Should not contain whitespaces.
+5.You cannot use resevred keywords from java.
+*/
 
     }
 }
