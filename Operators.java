6d144c7a187e02f728c8c2908c959e4b24bb4549
khawa <khavarjk@gmail.com>
2020-07-02 17:22:49 +0500

    Types of operators in java Arthimatic operators Assignment operators

---
+++ Operators.java
@@ -0,0 +1,31 @@
+
+public class Operators {
+
+    public static void main(String[] args) {
+        /*
+         * Operators in java Operand operator Operand = Result 4 + 4 = 8 Types of
+         * operators in java Arthimatic operators Assignment operators Logical operators
+         * Comparsion operators
+         */
+        // Arthimatic operator
+        int num1 = 55, num2 = 65;
+        // Assignment opertaor
+        num1 += 10;
+        System.out.println("the value of num1 + num2 is:");
+        System.out.println(num1 + num2);
+        System.out.println("the value of num1 - num2 is:");
+        System.out.println(num1 - num2);
+        System.out.println("the value of num1 * num2 is:");
+        System.out.println(num1 * num2);
+        System.out.println("the value of num1 / num2 is:");
+        System.out.println(num1 / num2);
+        System.out.print("the value of num1 % num2 is:");
+        System.out.println(num1 % num2);
+
+        System.out.println(num1++);
+        System.out.println(++num1);
+        System.out.println(num1--);
+        System.out.println(--num1);
+
+    }
+}
---
+++ Primitive.java
@@ -0,0 +1,19 @@
+
+public class Primitive {
+
+    public static void main(String[] args) {
+
+        /*
+         * Two types of Data Types. 1.Primitive byte(1 byte)short(2 bytes)int(4
+         * bytes)long (8bytes)float(4bytes) double(8bytes)boolean(1 bit)char (2 bytes)
+         * 2.Non Primitive or Reference Data type. is same object type.
+         */
+        byte u = 65;
+        double d = 45.86865676d;
+        System.out.println(u);
+        System.out.println(d);
+        char grade = 'A';
+        System.out.println(grade);
+
+    }
+}
