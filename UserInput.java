a703f15b8a2fc9873fb2a947b8cb9d68cc05d0ae
khawa <khavarjk@gmail.com>
2020-07-02 17:58:12 +0500

    taking user input java

---
+++ UserInput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class UserInput {
+
+    public static void main(String[] args) {
+
+        // Taking user input java
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter input");
+        String input = scan.nextLine();
+        System.out.println(input);
+    }
+}
