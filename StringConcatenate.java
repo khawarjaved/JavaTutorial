800475a1565831abd9a97b746d316b2f10268df2
khawa <khavarjk@gmail.com>
2020-07-02 20:05:30 +0500

    String Concatenate

---
+++ StringConcatenate.java
@@ -0,0 +1,11 @@
+
+public class StringConcatenate {
+
+    public static void main(String[] args) {
+        // Using String Concatenate
+        String name = "khawar jk";
+        String brother = "wajid jk";
+        System.out.println(name);
+        System.out.println(name + " from " + brother);
+    }
+}
