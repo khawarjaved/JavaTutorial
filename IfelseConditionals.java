34040fbadc3c13b7673acb1cb3d5209d33c12550
khawa <khavarjk@gmail.com>
2020-07-02 21:51:50 +0500

    elseif statment

--- IfelseConditionals.java
+++ IfelseConditionals.java
@@ -7,8 +7,11 @@ public class IfelseConditionals {
         System.out.println("Enter input");
         int age = scan.nextInt();
 
-        if (age < 5) {
-            System.out.println("You are not a kid");
+        if (age > 20) {
+            System.out.println("You are an adult");
+        } else if (age > 5) {
+            System.out.println("You are not  kid");
+
         } else {
             System.out.println("you are a kid");
         }
