fda4e010d4081fe637f316c18b0fd53b1182a200
khawa <khavarjk@gmail.com>
2020-07-03 15:26:17 +0500

    Array in java

---
+++ Array.java
@@ -0,0 +1,11 @@
+
+public class Array {
+
+    public static void main(final String[] args) {
+        // Array in java
+        int[] marks = { 10, 20, 30, 40 };
+        marks[2] = 80;
+        System.out.println(marks[0]);
+        System.out.println(marks[2]);
+    }
+}
